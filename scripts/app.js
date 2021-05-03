6eea1f9dd11197d5d26af6af90d902cf4995033d
Jovan <jovansandhu3@gmail.com>
2021-05-03 10:56:17 -0700

    added js for calculator dk what else i changed

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-const btns = document.querySelectorAll('#calculator button');
-const operators = ['+', '-', 'x', '÷'];
-=======
-
-const btns = document.querySelectorAll('#calculator button');
-const operators = ['+', '-', 'x', '÷'];
-
->>>>>>> 1354dfc233f27e5a325c83cf5e8bce5584c7866f
