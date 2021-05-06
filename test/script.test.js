309a1316783682b085aa54e4dc4e08176c99467b
Jolly <jmurphy106@my.bcit.ca>
2021-05-06 13:01:16 -0700

    added the eval error test case

--- test/script.test.js
+++ test/script.test.js
@@ -38,9 +38,45 @@ test('test eval', () => {
   require('../public/script.js');
 
   const inputScreen = document.querySelector('.screen');
-  calc.specAction('5++8', '=')
+  calc.specAction('5+8', '=')
 
-  expect(inputScreen.innerHTML).toBe('Syntax Error');
+  expect(inputScreen.innerHTML).toBe('13');
+});
+
+test('test eval error', () => {
+  document.body.innerHTML = `
+  <div>
+<main class="calcGrid">
+  <div class="screen">
+  </div>        
+    <button class="twoSpots" id="acBtn">AC</button>        
+    <button>DEL</button>        
+    <button>÷</button>        
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>        
+    <button>x</button>        
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>        
+    <button>+</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>        
+    <button class="twoSpots">=</button>
+</main>
+</div>
+<footer class="bottom">&copy;2021 Agile Development Team </footer>
+`;
+require('../public/script.js');
+
+const inputScreen = document.querySelector('.screen');
+calc.specAction('5++8', '=')
+
+expect(inputScreen.innerHTML).toBe('Syntax Error');
 });
 
 test('test DEL', () => {
